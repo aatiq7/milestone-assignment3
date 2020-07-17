5417c194458e760534833e8ca2728d99ebe6689e
aatiq7 <aatiq.rahman01@gmail.com>
2020-07-17 14:06:21 +0600

    fully done

--- assignment.js
+++ assignment.js
@@ -7,7 +7,7 @@ function feetToMile( feet ) {
     }
     else {
         var mile = feet / 5280;   // 1 mile = 5280 feet
-        var mileFixed = mile.toFixed(5);
+        var mileFixed = mile.toFixed(5);   // fixed the value of mile
         return mileFixed;
     }
 }
@@ -17,12 +17,15 @@ function feetToMile( feet ) {
 // Wood calculator---------------------------------------
 
 function woodCalculator( chair, table, bed ) {
+    var woodPerChair = 1;
+    var woodPerTable = 3;
+    var woodPerBed = 5;
 
     if ( (chair < 0) || (table < 0) || (bed < 0) ) {
-        return 'Sorry, input is invalid.';
+        return 'Sorry, input is invalid.';   // chair, table and bed numbers cannot be negative
     }
     else {
-        var totalWood = ( chair * 1 ) + ( table * 3 ) + ( bed * 5 );
+        var totalWood = ( chair * woodPerChair ) + ( table * woodPerTable ) + ( bed * woodPerBed );   // calculating total wood
         return totalWood;
     }
 }
@@ -39,21 +42,21 @@ function brickCalculator ( floor ) {
     var moreThenTwentyFloorHeight =10;
 
     if ( floor <= 0 ) {
-        return 'Floor number must be greater then 0.';
+        return 'Floor number must be greater then 0.';   // floor cannot be 0 or less then 0
     }
     else {
         if ( floor <=10 ) {
             var firstTenFloor = floor;
-            return firstTenFloor * firstTenFloorHeight * bricksPerFeet;
+            return firstTenFloor * firstTenFloorHeight * bricksPerFeet;   // calculate total bricks
         }
         else if ( floor > 10 && floor <=20 ) {
             tenToTwentyFloor = floor - 10;
-            return ( firstTenFloor * firstTenFloorHeight * bricksPerFeet ) + ( tenToTwentyFloor * tenToTwentyFloorHeight * bricksPerFeet );
+            return ( firstTenFloor * firstTenFloorHeight * bricksPerFeet ) + ( tenToTwentyFloor * tenToTwentyFloorHeight * bricksPerFeet );   // calculate total bricks
         }
         else {
             tenToTwentyFloor = 10;
             moreThenTwentyFloor = floor - 20;
-            return ( firstTenFloor * firstTenFloorHeight * bricksPerFeet ) + ( tenToTwentyFloor * tenToTwentyFloorHeight * bricksPerFeet ) + ( moreThenTwentyFloor * moreThenTwentyFloorHeight * bricksPerFeet );
+            return ( firstTenFloor * firstTenFloorHeight * bricksPerFeet ) + ( tenToTwentyFloor * tenToTwentyFloorHeight * bricksPerFeet ) + ( moreThenTwentyFloor * moreThenTwentyFloorHeight * bricksPerFeet );   // calculate total bricks
         }
     }
 }
@@ -63,17 +66,17 @@ function brickCalculator ( floor ) {
 // Tiny Friend-----------------------------------------
 
 function tinyFriend( friends ) {
-    var tiny = friends[0];
+    var tiny = friends[0];   // imagine friends[0] is the value of tiny
 
     if ( friends.length == 0 ) {
-        return 'Array is empty.';
+        return 'Array is empty.';   // if array is empty
     }
 
     for ( var i = 0; i < friends.length; i++ ) {
-        var length = friends[i].length;
+        var length = friends[i].length;   // finding the others friends[i] length in array
         
         if( length < tiny.length ) {
-            tiny = friends[i];
+            tiny = friends[i];   // if frineds[i] length smaller then tiny length then friend[i] will be tiny
         }
     }
     return tiny;
